b6545136f20ad67f4c605932d36450cfd7452dc2
Patrick <pmcl2472@uni.sydney.edu.au>
2022-11-27 23:38:15 +1100

    PM Updates 271122

--- BacktestRunFile.py
+++ BacktestRunFile.py
@@ -47,7 +47,7 @@ stop_loss = -1
 take_profit = 2
 factor = 1/100 #This can be 1/10000 for one pip if using 'Flat' limit, 1/100 for one percent if using 'Percentage' limits, and so on.
 broker_cost = 0.5 #NB this is now applied twice, on the way in and on the way out.
-guaranteed_sl = False #Whether limits are firm, and cannot be gapped by high/low price, or not.
+guaranteed_sl = False #Whether limits are firm and cannot be gapped by high/low price or not.
 dynamic_limits = True #Whether limits are reset with a stronger signal or not
 
 #Apply factor
